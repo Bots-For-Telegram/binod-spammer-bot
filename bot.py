11c1e26195138ec8c7c2207f30ee0078b3d1052f
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2020-09-02 14:43:39 +0530

    sleep added

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from telegram.ext import Updater , CommandHandler, MessageHandler, Filters
 import logging
 import os
+import time
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 TOKEN = os.environ.get("TOKEN")
 PORT = int(os.environ.get("PORT",5000))
@@ -8,8 +9,12 @@ APP = "binod-spammer-bot"
 updater = Updater(TOKEN,use_context=True)
 dispatcher = updater.dispatcher
 def spam(update,context):
-  for i in range(29):
-    context.bot.sendMessage(update.message.chat_id,text="Binod")
+  for j in range(5):
+    for i in range(20):
+      context.bot.sendMessage(update.message.chat_id,text="Binod")
+      time.sleep(0.9)
+    time.sleep(60)
+
 spam_handler = MessageHandler((~Filters.command),spam)
 dispatcher.add_handler(spam_handler)
 updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
