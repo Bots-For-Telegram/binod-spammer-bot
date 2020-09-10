6dafaac1af7b7a2292396219bbff2338a92eae53
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2020-09-10 19:42:52 +0530

    spamming rate 10times increased

--- bot.py
+++ bot.py
@@ -9,10 +9,13 @@ APP = os.environ.get("APP_NAME")
 updater = Updater(TOKEN,use_context=True)
 dispatcher = updater.dispatcher
 def spam(update,context):
-  for j in range(500):
+  for j in range(5000):
     for i in range(20):
-      context.bot.sendMessage(update.message.chat_id,text="Binod")
-    time.sleep(60)
+      try:
+        context.bot.sendMessage(update.message.chat_id,text="Binod")
+      except:
+        pass  
+  time.sleep(60)
 
 spam_handler = MessageHandler((~Filters.command),spam)
 dispatcher.add_handler(spam_handler)
